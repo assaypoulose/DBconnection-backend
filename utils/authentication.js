9c4d432db5d6b9107f7ad5da6560ce2d82374a56
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-04-15 14:48:08 +0200

    made a small change

--- utils/authentication.js
+++ utils/authentication.js
@@ -1,15 +1,16 @@
 const {expressjwt} = require('express-jwt');
 
+
 exports.requireSignIn = expressjwt({
     secret: process.env.SECRET_KEY,
     algorithms: ['HS256'],
     userProperty: 'auth'
 })
 
-exports.isAuth = (req, res, next) => {
-    const user = (req.auth._id === req.params.userID);
+exports.isAuth = (req, res, next) =>{
+    const user = req.auth._id === req.profile._id.toString();
     if(!user){
-        return res.status(401).send('Access Denied! Signin again.')
+        return res.status(401).send('Access Denied! Sign in again.')
     }
 
     next();
